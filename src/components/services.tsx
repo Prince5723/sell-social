4519c71eed30659f407998d93ff25d2ba0ce60ba
Prince5723 <57prince23@gmail.com>
2025-04-01 11:02:57 +0530

    remvoed links from services

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -13,39 +13,39 @@ export function Services() {
 }
 export const projects = [
   {
-    title: "Branding",
-    description:
-      "Crafting powerful brand identities that connect and last. More than a logo, a resonating brand story. Boost visibility, and build appeal.",
-    link: "https://stripe.com",
-  },
-  {
-    title: "UI/UX Design",
-    description:
-      "We craft captivating digital experiences through user-centric UX/UI design. Combining aesthetics and functionality to drive meaningful interactions.",
-    link: "https://netflix.com",
-  },
-  {
     title: "MVP Development",
     description:
       "Unleash your vision with our MVP development services. From concept to launch, we craft digital solutions that propel your business forward.",
-    link: "https://meta.com",
+    link: "",
   },
   {
     title: "Website Development",
     description:
       "Crafting websites that impress and perform, combining thoughtful design, seamless navigation, and captivating visuals.",
-    link: "https://google.com",
+    link: "",
   },
   {
     title: "App Development",
     description:
       "Drive business growth with our custom app development services. We build innovative mobile applications from concept to launch.",
-    link: "https://amazon.com",
+    link: "",
+  },
+  {
+    title: "Branding",
+    description:
+      "Crafting powerful brand identities that connect and last. More than a logo, a resonating brand story. Boost visibility, and build appeal.",
+    link: "",
+  },
+  {
+    title: "UI/UX Design",
+    description:
+      "We craft captivating digital experiences through user-centric UX/UI design. Combining aesthetics and functionality to drive meaningful interactions.",
+    link: "",
   },
   {
     title: "Digital Marketing",
     description:
       "Unleash your brand’s digital power with us. Compelling content, strategic campaigns, and expert execution, tailor made for your brand.",
-    link: "https://microsoft.com",
+    link: "",
   },
 ];
