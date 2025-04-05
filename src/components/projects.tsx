93d9fd64497220cadfc66f6b8be7775302156faa
Prince5723 <57prince23@gmail.com>
2025-04-05 21:55:16 +0530

    fixed height of images

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -17,31 +17,33 @@ export const LayoutGrid = ({ cards }: { cards: Card[] }) => {
   };
 
   return (
-    <div className="w-full h-full p-10  grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto gap-4">
-      {cards.map((card, i) => (
-        <div
-          key={i}
-          className={cn(
-            card.id === 1 || card.id === 4 || card.id === 5 ? "md:col-span-2" : "col-span-1",
-            "bg-white rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 cursor-pointer relative h-64"
-          )}
-          onClick={() => handleCardClick(card.link)}
-        >
-          <div className="w-full h-full border boder-slate-50 rounded-lg overflow-hidden">
-            <img
-              src={card.thumbnail}
-              alt={card.title}
-              className="object-cover object-top h-full w-full transition-all duration-500 ease-in-out transform hover:scale-110"
-            />
-          </div>
-          <div className="absolute inset-0  flex flex-col justify-start p-6 pointer-events-none">
-            <h3 className="font-bold text-xl text-gray-200">{card.title}</h3>
-            <p className="text-gray-400 text-base line-clamp-3">{card.description}</p>
-          </div>
+  <div className="w-full h-full p-10 grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto gap-4">
+    {cards.map((card, i) => (
+      <div
+        key={i}
+        className={cn(
+          card.id === 1 || card.id === 4 || card.id === 5 ? "md:col-span-2" : "col-span-1",
+          "bg-white rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 cursor-pointer relative md:h-64"
+        )}
+        onClick={() => handleCardClick(card.link)}
+      >
+        <div className="w-full h-full border border-slate-50 rounded-lg overflow-hidden">
+          <img
+            src={card.thumbnail}
+            alt={card.title}
+            className="object-contain md:object-cover object-top h-full w-full transition-all duration-500 ease-in-out transform hover:scale-110"
+          />
         </div>
-      ))}
-    </div>
-  );
+        <div className="absolute inset-0 flex flex-col justify-start p-6 pointer-events-none">
+          <h3 className="font-bold text-xl text-gray-200">{card.title}</h3>
+          <p className="text-gray-400 text-base line-clamp-3">{card.description}</p>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+  
 };
 
 export function Projects() {
